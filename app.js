066fb7709617c1b4877180a2d800fc44d1f63ff0
cotemanuel1997 <cotemanuel6@gmail.com>
2025-07-31 00:52:50 -0300

    add comments explaining functionality

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Fucionalidades: 
+
+// Agregar nombres: 
+
+//  Los usuarios escribirán el nombre de un amigo en un campo de texto 
+
+ 
+
+// Validar entrada: 
+
+//  Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido. 
+
+// Visualizar la lista: 
+
+//  Los nombres ingresados aparecerán en una lista debajo del campo de entrada. 
+
+// se agregarán a una lista visible al hacer clic en "Adicionar". 
+
+// Los nombres ingresados se mostrarán en una lista visible en la página, 
+
+ 
+
+// Sorteo aleatorio: 
+
+//  Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página. 
+
+// determinar quién es el "amigo secreto". 
